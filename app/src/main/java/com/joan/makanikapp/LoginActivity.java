9e33cbf6a7c579dd5b90332bcd3e0e172a388e88
JoanWaweru <joan.waweru@strathmore.edu>
2021-07-16 21:09:19 +0300

    Initial Commit

---
+++ app/src/main/java/com/joan/makanikapp/LoginActivity.java
@@ -0,0 +1,30 @@
+package com.joan.makanikapp;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+//import coding.insight.cleanuiloginregister.R;
+
+
+public class LoginActivity extends AppCompatActivity {
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            //for changing status bar icon colors
+            if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.M){
+                getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+            }
+            setContentView(R.layout.activity_login);
+        }
+
+        public void onLoginClick(View View){
+            startActivity(new Intent(this,RegisterActivity.class));
+            overridePendingTransition(R.anim.slide_in_right,R.anim.stay);
+
+        }
+    }
+
