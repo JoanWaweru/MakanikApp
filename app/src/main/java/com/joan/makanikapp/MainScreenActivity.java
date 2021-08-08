8fc57d378b62f47b7aee00b09cc2d1d2d7110eaf
JoanWaweru <joan.waweru@strathmore.edu>
2021-08-09 00:25:34 +0300

    Main Screen Opens New Intents

--- app/src/main/java/com/joan/makanikapp/MainScreenActivity.java
+++ app/src/main/java/com/joan/makanikapp/MainScreenActivity.java
@@ -1,13 +1,60 @@
 package com.joan.makanikapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
+import androidx.cardview.widget.CardView;
 
-public class MainScreenActivity extends AppCompatActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainScreenActivity extends AppCompatActivity implements View.OnClickListener {
+    public CardView callMechanic,viewProfile,signAsMechanic,logout;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+
+        callMechanic = findViewById(R.id.callMechanicCard);
+        viewProfile = findViewById(R.id.viewProfileCard);
+        signAsMechanic = findViewById(R.id.signAsMechanicCard);
+        logout = findViewById(R.id.logoutCard);
+
+        callMechanic.setOnClickListener(this);
+        viewProfile.setOnClickListener(this);
+        signAsMechanic.setOnClickListener(this);
+        logout.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i;
+        switch (v.getId()){
+            case R.id.callMechanicCard:
+                i = new Intent(this,NavigationDrawerActivity.class);
+                startActivity(i);
+                break;
+
+            case R.id.viewProfileCard:
+                i = new Intent(this,ViewProfileActivity.class);
+                startActivity(i);
+                break;
+
+            case R.id.signAsMechanicCard:
+                i = new Intent(this,MechanicRegisterActivity.class);
+                startActivity(i);
+                break;
+
+            case R.id.logoutCard:
+                mAuth = FirebaseAuth.getInstance();
+                mAuth.signOut();
+                i = new Intent(this,LoginActivity.class);
+                startActivity(i);
+                break;
+        }
+
     }
 }
--- app/src/main/java/com/joan/makanikapp/SplashScreenActivity.java
+++ app/src/main/java/com/joan/makanikapp/SplashScreenActivity.java
@@ -1,29 +1,47 @@
 package com.joan.makanikapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
-        Handler handler;
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_splash_screen);
+    private static int SPLASH_SCREEN = 5000;
 
-            handler=new Handler();
-            handler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    Intent intent=new Intent(SplashScreenActivity.this,MainScreenActivity.class);
-                    startActivity(intent);
-                    finish();
-                }
-            },3000);
+    Animation topAnim,bottomAnim;
+    ImageView splashImage;
+//    TextView splashDescription;
 
-        }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        splashImage = findViewById(R.id.logo);
+//        splashDescription = findViewById(R.id.splashIntro);
+
+        splashImage.setAnimation(topAnim);
+//        splashDescription.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this,LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN);
     }
+}
