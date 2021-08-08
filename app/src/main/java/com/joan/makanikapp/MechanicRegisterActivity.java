fc4e8ba7b890d73695ce9995211d2837763ee153
JoanWaweru <joan.waweru@strathmore.edu>
2021-08-08 22:03:18 +0300

    Dashboard Screen Added

---
+++ app/src/main/java/com/joan/makanikapp/MechanicRegisterActivity.java
@@ -0,0 +1,14 @@
+package com.joan.makanikapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MechanicRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mechanic_register);
+    }
+}
